90ed1c4751be777ce0acdd213f5d1cf3f4556bb0
Ansh Ramanath <anshramanath04@gmail.com>
2025-05-23 17:00:58 -0500

    Initial Commit

---
+++ HealthAI/HealthAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAIApp.swift
+//  HealthAI
+//
+//  Created by Ansh Ramanath on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthAITests/HealthAITests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAITests.swift
+//  HealthAITests
+//
+//  Created by Ansh Ramanath on 5/23/25.
+//
+
+import Testing
+@testable import HealthAI
+
+struct HealthAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
